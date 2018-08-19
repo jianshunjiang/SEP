6ea7ec1fd85c78eb6b90a1ca8628f77cb81142a2
leighton070103 <leighton070103@gmail.com>
2018-08-19 14:24:37 +1000

    Delete idea configuration files

---
+++ base/src/main/java/com/loan/uts/model/Application.java
@@ -0,0 +1,144 @@
+package com.loan.uts.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Entity
+public class Application {
+    private int id;
+    private String status;
+    private Integer staffId;
+    private Integer applicantId;
+    private Date submitDate;
+    private Date completeDate;
+    private String title;
+    private String content;
+    private String attachment;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "status", nullable = true, length = 10)
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Basic
+    @Column(name = "staff_id", nullable = true)
+    public Integer getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(Integer staffId) {
+        this.staffId = staffId;
+    }
+
+    @Basic
+    @Column(name = "applicant_id", nullable = true)
+    public Integer getApplicantId() {
+        return applicantId;
+    }
+
+    public void setApplicantId(Integer applicantId) {
+        this.applicantId = applicantId;
+    }
+
+    @Basic
+    @Column(name = "submit_date", nullable = true)
+    public Date getSubmitDate() {
+        return submitDate;
+    }
+
+    public void setSubmitDate(Date submitDate) {
+        this.submitDate = submitDate;
+    }
+
+    @Basic
+    @Column(name = "complete_date", nullable = true)
+    public Date getCompleteDate() {
+        return completeDate;
+    }
+
+    public void setCompleteDate(Date completeDate) {
+        this.completeDate = completeDate;
+    }
+
+    @Basic
+    @Column(name = "title", nullable = true, length = 50)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Basic
+    @Column(name = "content", nullable = true, length = 200)
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Basic
+    @Column(name = "attachment", nullable = true, length = 100)
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Application that = (Application) o;
+
+        if (id != that.id) return false;
+        if (status != null ? !status.equals(that.status) : that.status != null) return false;
+        if (staffId != null ? !staffId.equals(that.staffId) : that.staffId != null) return false;
+        if (applicantId != null ? !applicantId.equals(that.applicantId) : that.applicantId != null) return false;
+        if (submitDate != null ? !submitDate.equals(that.submitDate) : that.submitDate != null) return false;
+        if (completeDate != null ? !completeDate.equals(that.completeDate) : that.completeDate != null) return false;
+        if (title != null ? !title.equals(that.title) : that.title != null) return false;
+        if (content != null ? !content.equals(that.content) : that.content != null) return false;
+        if (attachment != null ? !attachment.equals(that.attachment) : that.attachment != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (staffId != null ? staffId.hashCode() : 0);
+        result = 31 * result + (applicantId != null ? applicantId.hashCode() : 0);
+        result = 31 * result + (submitDate != null ? submitDate.hashCode() : 0);
+        result = 31 * result + (completeDate != null ? completeDate.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        result = 31 * result + (attachment != null ? attachment.hashCode() : 0);
+        return result;
+    }
+}
---
+++ base/src/main/java/com/loan/uts/model/Student.java
@@ -0,0 +1,104 @@
+package com.loan.uts.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    private int id;
+    private String password;
+    private String name;
+    private String bankaccount;
+    private String email;
+    private String phone;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "password", nullable = false, length = 50)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = true, length = 20)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "bankaccount", nullable = true, length = 100)
+    public String getBankaccount() {
+        return bankaccount;
+    }
+
+    public void setBankaccount(String bankaccount) {
+        this.bankaccount = bankaccount;
+    }
+
+    @Basic
+    @Column(name = "email", nullable = false, length = 100)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "phone", nullable = true, length = 50)
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        if (id != student.id) return false;
+        if (password != null ? !password.equals(student.password) : student.password != null) return false;
+        if (name != null ? !name.equals(student.name) : student.name != null) return false;
+        if (bankaccount != null ? !bankaccount.equals(student.bankaccount) : student.bankaccount != null) return false;
+        if (email != null ? !email.equals(student.email) : student.email != null) return false;
+        if (phone != null ? !phone.equals(student.phone) : student.phone != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (bankaccount != null ? bankaccount.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        return result;
+    }
+}
