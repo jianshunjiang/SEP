d56367cc2ad47aa783e6630999164bf5e9e9ef31
leighton070103 <leighton070103@gmail.com>
2018-08-29 15:12:21 +1000

    Completed database schema

--- uts_loan.sql
+++ uts_loan.sql
@@ -16,63 +16,28 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Application`
+-- Table structure for table `admin`
 --
 
-DROP TABLE IF EXISTS `Application`;
+DROP TABLE IF EXISTS `admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `Application` (
-  `id` int(11) NOT NULL,
-  `applicant_id` int(11) DEFAULT NULL,
-  `attachment` varchar(100) DEFAULT NULL,
-  `complete_date` datetime DEFAULT NULL,
-  `content` varchar(200) DEFAULT NULL,
-  `staff_id` int(11) DEFAULT NULL,
-  `status` varchar(10) DEFAULT NULL,
-  `submit_date` datetime DEFAULT NULL,
-  `title` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK7maykjjlryndadklx2rg7v6cl` (`applicant_id`),
-  CONSTRAINT `FK7maykjjlryndadklx2rg7v6cl` FOREIGN KEY (`applicant_id`) REFERENCES `student` (`id`),
-  CONSTRAINT `FKtbuiidnmel5ub6sj56r5lvvp3` FOREIGN KEY (`applicant_id`) REFERENCES `Student` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Application`
---
-
-LOCK TABLES `Application` WRITE;
-/*!40000 ALTER TABLE `Application` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Application` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Student`
---
-
-DROP TABLE IF EXISTS `Student`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `Student` (
-  `id` int(11) NOT NULL,
-  `bankaccount` varchar(100) DEFAULT NULL,
-  `email` varchar(100) NOT NULL,
-  `name` varchar(20) DEFAULT NULL,
+CREATE TABLE `admin` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `phone` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Student`
+-- Dumping data for table `admin`
 --
 
-LOCK TABLES `Student` WRITE;
-/*!40000 ALTER TABLE `Student` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Student` ENABLE KEYS */;
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` VALUES (1,'admin','111'),(2,'adminTest','111');
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -83,18 +48,18 @@ DROP TABLE IF EXISTS `application`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `application` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `status` varchar(10) DEFAULT NULL,
-  `staff_id` int(11) DEFAULT NULL,
-  `applicant_id` int(11) DEFAULT NULL,
+  `manager_id` int(11) DEFAULT NULL,
   `submit_date` date DEFAULT NULL,
-  `complete_date` date DEFAULT NULL,
-  `title` varchar(50) DEFAULT NULL,
-  `content` varchar(200) DEFAULT NULL,
-  `attachment` varchar(100) DEFAULT NULL,
+  `result_date` date DEFAULT NULL,
+  `content` varchar(5000) DEFAULT NULL,
+  `student_id` int(8) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `applicant_id` (`applicant_id`),
-  CONSTRAINT `application_ibfk_1` FOREIGN KEY (`applicant_id`) REFERENCES `student` (`id`)
+  KEY `application_student__fk` (`student_id`),
+  KEY `application_manager__fk` (`manager_id`),
+  CONSTRAINT `application_manager__fk` FOREIGN KEY (`manager_id`) REFERENCES `manager` (`id`),
+  CONSTRAINT `application_student__fk` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -108,6 +73,89 @@ LOCK TABLES `application` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `attachment`
+--
+
+DROP TABLE IF EXISTS `attachment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `attachment` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `draft_id` int(10) DEFAULT NULL,
+  `application_id` int(10) DEFAULT NULL,
+  `upload_date` date DEFAULT NULL,
+  `path` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `attachment`
+--
+
+LOCK TABLES `attachment` WRITE;
+/*!40000 ALTER TABLE `attachment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attachment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `draft`
+--
+
+DROP TABLE IF EXISTS `draft`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `draft` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `content` varchar(5000) DEFAULT NULL,
+  `student_id` int(10) DEFAULT NULL,
+  `last_edit` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `draft`
+--
+
+LOCK TABLES `draft` WRITE;
+/*!40000 ALTER TABLE `draft` DISABLE KEYS */;
+/*!40000 ALTER TABLE `draft` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manager`
+--
+
+DROP TABLE IF EXISTS `manager`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `manager` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `fistname` varchar(10) DEFAULT NULL,
+  `lastname` varchar(10) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `mobile` varchar(20) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `delete` tinyint(1) NOT NULL DEFAULT '0',
+  `admin_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `manager_admin__fk` (`admin_id`),
+  CONSTRAINT `manager_admin__fk` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manager`
+--
+
+LOCK TABLES `manager` WRITE;
+/*!40000 ALTER TABLE `manager` DISABLE KEYS */;
+INSERT INTO `manager` VALUES (1,'Manager','Test','12416881@student.uts.edu.au','12344556','111',0,1),(2,'Olo','New','leighton070103@gmail.com','1234555','111',0,1);
+/*!40000 ALTER TABLE `manager` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `student`
 --
 
@@ -117,10 +165,17 @@ DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student` (
   `id` int(10) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `name` varchar(20) DEFAULT NULL,
   `bankaccount` varchar(100) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
   `phone` varchar(50) DEFAULT NULL,
+  `faculty` varchar(4) DEFAULT NULL,
+  `firstname` varchar(10) DEFAULT NULL,
+  `lastname` varchar(10) DEFAULT NULL,
+  `course` varchar(6) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `gender` varchar(6) DEFAULT NULL,
+  `nationality` varchar(20) DEFAULT NULL,
+  `start_year` varchar(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -131,7 +186,7 @@ CREATE TABLE `student` (
 
 LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES (12345678,'12345678','test account','00000000','might_103@yeah.net','1111111'),(87654321,'87654321','Ayman','Tree new bee','shunj110@gmail.com','202929292');
+INSERT INTO `student` VALUES (12345678,'12345678','00000000','might_103@yeah.net','1111111','FEIT','Ayman','Jiang','C8990','1989-08-18','Male','mongo','2015'),(87654321,'87654321','Tree new bee','shunj110@gmail.com','202929292','FASS','Spring','Test','D909','2015-08-07','Female','Australia','2011');
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -144,4 +199,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-08-22 16:02:09
+-- Dump completed on 2018-08-29 15:11:38
