9f1aefdad3aaee26ec3556918c2513b5ed2d5aab
axlrose0308 <stradlin0518@gmail.com>
2018-10-03 19:08:03 +1000

    Encryption

--- uts.loan/src/main/java/com/loan/uts/util/EncrptUtil.java
+++ uts.loan/src/main/java/com/loan/uts/util/EncrptUtil.java
@@ -1,8 +1,36 @@
 package com.loan.uts.util;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class EncrptUtil {
 
+    public void Encrypt(){
+        PasswordEncoder password = new BCryptPasswordEncoder();
+        for(int i = 0; i < 5; ++i){
+            String encrypted = password.encode("PaSsWoRd");
+
+            if(password.matches("PaSsWoRd", encrypted)) {
+
+            }
+            else {
+
+            }
+        }
+    }
+
     public static String encrypt(String s){
+
+        String password = "testpassword";
+        String hashed = BCrypt.hashpw(password, BCrypt.gensalt(10));
+        String candidate = "testpassword";
+
+        if(BCrypt.checkpw(candidate, hashed)){
+
+        }
+        else {
+
+        }
         return "";
     }
 }
